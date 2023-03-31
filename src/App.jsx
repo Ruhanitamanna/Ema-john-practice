9099c07edaeeb678d3403318fc66e1092fb273fe
Ruhanitamanna <tamannanasrin01@gmail.com>
2023-03-31 11:44:23 +0600

    added product

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 
 import './App.css'
 import Header from './components/Header/Header'
+import Shop from './components/Shop/Shop'
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
   return (
     <div className="App">
     <Header></Header>
+    <Shop></Shop>
     </div>
   )
 }
